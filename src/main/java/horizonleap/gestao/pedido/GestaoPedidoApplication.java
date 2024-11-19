934726f22d491011f18cbc6795023acd464d1e52
Pedro Plá <pedro.bpla@gmail.com>
2024-11-19 20:34:59 -0300

    commit inicial do Spring Initializr

---
+++ src/main/java/horizonleap/gestao/pedido/GestaoPedidoApplication.java
@@ -0,0 +1,13 @@
+package horizonleap.gestao.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/horizonleap/gestao/pedido/GestaoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package horizonleap.gestao.pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
